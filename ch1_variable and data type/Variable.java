d541e116a35b0d0ffbd73d66294bb5cdc187b64d
naitikpandey7514-spec <naitikpandey7514@gmail.com>
2026-07-24 12:31:50 +0530

    change

---
+++ ch1_variable and data type/Variable.java
@@ -0,0 +1,21 @@
+// variable - variable is a name used to store data in the memory.
+import java.util.*;
+class Variable{
+    public static void main(String[] args) {
+        String name ="naitik";   // name is  a variable
+        int age = 19;
+        System.out.println(name);
+        System.out.println(age);
+        // SUM OF TWO NUMBER
+        Scanner sc = new Scanner(System.in);
+         int a = sc.nextInt();
+        int b = sc.nextInt();
+        int sum = a + b;
+        System.out.println(sum);
+
+
+    }
+}
+
+
+
---
+++ ch1_variable and data type/data_type.java
@@ -0,0 +1,23 @@
+// data type primitive
+
+public class data_type {
+    public static void main(String[] args){
+                byte age = 20;  // 1bytes
+                short year = 2026; // 2 bytes
+                int marks = 95;       //integer 2bytes
+                long population = 8000000000L; //for long integer 8 bytes
+                float price = 99.99f;  //decimal value 4 bytes
+                double pi = 3.14159;  // long decimal value 8 bytes
+                char grade = 'A';    //character 2 bytes
+                boolean passed = true;  // true/false 1bit
+
+                System.out.println(age);
+                System.out.println(year);
+                System.out.println(marks);
+                System.out.println(population);
+                System.out.println(price);
+                System.out.println(pi);
+                System.out.println(grade);
+                System.out.println(passed);
+            }
+    }
