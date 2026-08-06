27f697b601a075b8fbeea60e07d6f2986ff3e030
naitikpandey7514-spec <naitikpandey7514@gmail.com>
2026-08-06 22:29:55 +0530

    change

--- ch5_array/array.java
+++ ch5_array/array.java
@@ -1,4 +1,37 @@
-package PACKAGE_NAME;
+// array - An array is a collection of elements of the same data type stored in contiguous memory locations. Each element is accessed using an index, starting from 0.
+
+
+/*
+
+syntax
+
+
+declaration:
+
+datatype[] arrName;
+
+creation:
+
+datatype[] arrName = new datatype[size];
+
+initialization:
+datatype[] arrName = value;
+declare,create,initialise
+dataType[] arrayName = {value1, value2, value3, ...};
+
+ */
+
+
+//eg
 
 public class array {
+    public static void main(String[] args) {
+
+        int[] numbers = {10,20,30,40,50};
+
+        for(int i=0;i<numbers.length;i++)
+        {
+            System.out.println(numbers[i]);
+        }
+    }
 }
